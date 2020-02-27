bb107e83b72d9416df4823d7d177cca4a49b2694
secretshardul <shardul.aeer@somaiya.edu>
2020-02-27 18:08:38 +0530

    feat: Query single book details

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -2,8 +2,16 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import { getBooksQuery } from "../queries/queries";
 
+//components
+import BookDetails from "./BookDetails";
 
 class BookList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selected: null
+        }
+    }
 
     displayBooks(){
         let data = this.props.data;
@@ -11,7 +19,7 @@ class BookList extends Component {
             return (<div>Loading...</div>);
         } else {
             return data.books.map(book => {
-                return (<li key = { book.id }>{ book.name }</li>)
+                return (<li key = { book.id } onClick={ event => { this.setState({ selected: book.id }) } }>{ book.name }</li>)
             })
         }
     }
@@ -21,6 +29,7 @@ class BookList extends Component {
                 <ul id="book-list">
                     { this.displayBooks() }
                 </ul>
+                <BookDetails bookId={ this.state.selected }/>
             </div>
         )
     }
